a2f68c86dd7652dd727c492e170b7373462deefe
Mohamad Al Salty <mohamadhazems@gmail.com>
2024-06-25 20:21:43 -0300

    fix: cors issue

--- routes/routes.go
+++ routes/routes.go
@@ -6,12 +6,27 @@ import (
 )
 
 func SetupRoutes() {
-	http.HandleFunc("/clusterinfo", handlers.ClusterInfoHandler)
-	http.HandleFunc("/namespaces", handlers.NamespacesHandler)
-	http.HandleFunc("/nodes", handlers.NodesHandler)
-	http.HandleFunc("/pods/", handlers.PodsHandler)
-	http.HandleFunc("/deployments/", handlers.DeploymentsHandler)
-	http.HandleFunc("/replicasets/", handlers.ReplicaSetsHandler)
-	http.HandleFunc("/daemonsets/", handlers.DaemonSetsHandler)
+	http.HandleFunc("/clusterinfo", enableCors(handlers.ClusterInfoHandler))
+	http.HandleFunc("/namespaces", enableCors(handlers.NamespacesHandler))
+	http.HandleFunc("/nodes", enableCors(handlers.NodesHandler))
+	http.HandleFunc("/pods/", enableCors(handlers.PodsHandler))
+	http.HandleFunc("/deployments/", enableCors(handlers.DeploymentsHandler))
+	http.HandleFunc("/replicasets/", enableCors(handlers.ReplicaSetsHandler))
+	http.HandleFunc("/daemonsets/", enableCors(handlers.DaemonSetsHandler))
+}
 
+// enableCors is a middleware function to enable CORS for handlers
+func enableCors(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		handler.ServeHTTP(w, r)
+	}
 }
